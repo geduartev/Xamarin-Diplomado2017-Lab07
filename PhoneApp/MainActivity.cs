18007dd1634e7a1dc7fe9f676073aa4efbabbd14
Gabriel Eduardo Duarte Vega <geduartev@gmail.com>
2017-06-15 11:57:40 -0500

    Add project files.

---
+++ PhoneApp/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PhoneApp
+{
+    [Activity(Label = "Phone App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        static readonly System.Collections.Generic.List<string> PhoneNumbers = new System.Collections.Generic.List<string>();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberEditText);
+            var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var CallButton = FindViewById<Button>(Resource.Id.CallButton);
+            var CallHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            var ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+
+            CallButton.Enabled = false;
+
+            var TranslatedNumber = string.Empty;
+
+            TranslateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    CallButton.Text = "Llamar";
+                    CallButton.Enabled = false;
+                }
+
+                if (!string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    CallButton.Text = $"Llamar al {TranslatedNumber}";
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var CallDialog = new AlertDialog.Builder(this);
+                CallDialog.SetMessage($"Llamar al número {TranslatedNumber}?");
+                CallDialog.SetNeutralButton("Llamar", delegate
+                {
+                    PhoneNumbers.Add(TranslatedNumber);
+                    CallHistoryButton.Enabled = true;
+
+                    var CallIntent = new Android.Content.Intent(Android.Content.Intent.ActionCall);
+                    CallIntent.SetData(Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+                    StartActivity(CallIntent);
+                });
+                CallDialog.SetNegativeButton("Cancelar", delegate { });
+                CallDialog.Show();
+            };
+
+            CallHistoryButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var AndroidContentIntent = new Android.Content.Intent(this, typeof(CallHistoryActivity));
+
+                AndroidContentIntent.PutStringArrayListExtra("phone_numbers", PhoneNumbers);
+
+                StartActivity(AndroidContentIntent);
+            };
+
+            ValidateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var ValidateIntent = new Android.Content.Intent(this, typeof(ValidateActivity));
+                StartActivity(ValidateIntent);
+            };
+        }
+    }
+}
+
---
+++ PhoneApp/ValidateActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneApp
+{
+    [Activity(Label = "@string/ValidateActivity", MainLauncher = false, Icon = "@drawable/icon")]
+    public class ValidateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Validate);
+
+            var ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+
+            ValidateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                Validate();
+            };
+        }
+
+        private async void Validate()
+        {
+            var Email = FindViewById<EditText>(Resource.Id.EmailEditText);
+            var Password = FindViewById<EditText>(Resource.Id.PasswordEditText);
+
+            string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+            var ServiceClient = new SALLab07.ServiceClient();
+            var Result = await ServiceClient.ValidateAsync(Email.Text, Password.Text, myDevice);
+
+            if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var notification = new Notification.Builder(this)
+                    .SetContentTitle("Validación de actividad")
+                    .SetContentText($"{Result.Status}\n{Result.Fullname}\n{Result.Token}")
+                    .SetSmallIcon(Resource.Drawable.Icon);
+
+                notification.SetCategory(Notification.CategoryMessage);
+
+                var ObjectNotification = notification.Build();
+
+                var Manager = GetSystemService(
+                    Android.Content.Context.NotificationService) as NotificationManager;
+
+                Manager.Notify(0, ObjectNotification);
+            }
+            else
+            {
+                var ValidateMessage = FindViewById<TextView>(Resource.Id.ValidateMessageTextView);
+                ValidateMessage.SetPadding(40, 20, 0, 0);
+                ValidateMessage.Text = $"{Result.Status}\n{Result.Fullname}\n{Result.Token}";
+            }
+
+        }
+    }
+}
